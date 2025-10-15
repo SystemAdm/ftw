3a3d9e5110ec8d335a2f0e0e39642eeec3a07f6e
odd-erik <jovang@gmail.com>
2025-10-15 16:45:30 +0200

    .

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -3,16 +3,16 @@ import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { spillexpo } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
 import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
+import { dashboard } from '@/routes/admin';
 
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
-        href: spillexpo(),
+        href: dashboard(),
         icon: LayoutGrid,
     },
 ];
@@ -37,7 +37,7 @@ const footerNavItems: NavItem[] = [
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
-                        <Link :href="spillexpo()">
+                        <Link :href="dashboard()">
                             <AppLogo />
                         </Link>
                     </SidebarMenuButton>
